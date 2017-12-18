3fb5fbbbb231b0db88590d316725c9755cb7f4f4
Sarang <sarang@accedius.com>
2017-12-18 23:51:59 +0000

    Added Comments

--- test/test_learning.py
+++ test/test_learning.py
@@ -1,11 +1,16 @@
+# add imports
 import unittest
-
 import sysconfig as sys
+
+# Class to contain all the Unit Test for Python
+# the runner for the test is nosetest
 
 class TddWithPython(unittest.TestCase):
     def test_python_env(self):
         self.assertEquals('posix', sys.os.name)
+
         
-        
+
+# this line is required for running the python code        
 if __name__ == '__main__':
     unittest.main()
